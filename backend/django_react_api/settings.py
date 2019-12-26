81614fe2ad20364d9bfd89eff4071a102046b69a
Roar-Song <shh331@naver.com>
2019-12-26 18:48:32 +0900

    Create django superuser and Add test post

--- backend/django_react_api/settings.py
+++ backend/django_react_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'post',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# rest_framework add
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
